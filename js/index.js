bf38d2274602e397a251b7197ddd6002ccc132fb
Valterfi <morpheuw@gmail.com>
2020-04-27 02:35:31 -0700

    starting challenge form

--- js/index.js
+++ js/index.js
@@ -6,6 +6,19 @@ $(function () {
         contain: true
       });
 
+    let v = "";
+    $("#password").on('input', function () {
+        v = $(this).val();
+
+        if (v === '123') {
+            $(".secret-div").show('slow');
+        } else {
+            $(".secret-div").hide('slow');
+        }
+
+        console.log("value = " + v);
+    });
+
     // $.ajax({
     //     method: "GET",
     //     url: "https://esther-birthday.herokuapp.com/test"
@@ -14,6 +27,5 @@ $(function () {
     //         alert('deu certo');
     //     });
 
-    console.log('teste 2131321');
 
 });

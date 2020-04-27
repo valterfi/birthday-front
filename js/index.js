5405480bda87d677e040df6f29a2835caa705b4b
Valterfi <morpheuw@gmail.com>
2020-04-27 09:33:12 -0700

    implementing all steps

--- js/index.js
+++ js/index.js
@@ -1,35 +1,90 @@
 $(function () {
 
-    $('.main-carousel').flickity({
-        // options
-        cellAlign: 'left',
-        contain: true
-      });
+    //let urlBase = 'https://esther-birthday.herokuapp.com'
+    let urlBase = 'http://localhost:8080'
 
-    let v = "";
-    $("#password").on('input', function () {
-        v = $(this).val();
 
-        if (v === '123') {
-            $(".secret-div").show({
-                duration: 1000
-            });
-        } else {
-            $(".secret-div").hide({
-                duration: 1000
-            });
-        }
+    $.ajax({
+        method: 'GET',
+        url: urlBase + '/startStep'
+      })
+        .done(function(data) {
+            main(data);
+        });
 
-        console.log("value = " + v);
-    });
+    function main(result) {
 
-    // $.ajax({
-    //     method: "GET",
-    //     url: "https://esther-birthday.herokuapp.com/test"
-    //   })
-    //     .done(function() {
-    //         alert('deu certo');
-    //     });
+        console.log(result);
+
+        $('.main-carousel').flickity({
+            // options
+            cellAlign: 'left',
+            contain: true
+        });
+
+        console.log('chamou aqui1');
+        updateCheckSecret(result);
+       
+        updateClickButton(result);
+
+    }
+
+    function updateCheckSecret(result) {
+        let secretKey = "";
+        $("#password").on('input', function (e) {
+            e.preventDefault();
+            secretKey = $(this).val();
+
+            if (secretKey === result.currentSecretKey) {
+
+                $.ajax({
+                    method: 'GET',
+                    url: urlBase + '/getSecrets'
+                })
+                    .done(function(secretData) {
+                        
+                        $(".secret-image").attr("src", secretData.secretImagePath);
+                        $(".secret-description").text(secretData.secretDescription);
+                        
+                        if (result.currentStep < 6) {
+                            $(".secret-button").text(secretData.secretButton);
+                        } else {
+                            $(".secret-button").hide();
+                        }   
+
+                        if (secretData.secretExtra) {
+                            $('.secret-extra').append(secretData.secretExtra);
+                        }
+
+                        $('.secret-div').show({
+                            duration: 1000
+                        });
+                    });
+               
+            } else {
+                $('.secret-div').hide({
+                    duration: 1000
+                });
+            }
+        });
+    }
+
+    function updateClickButton(result) {
+        $('.secret-button').click(function(e) {
+            e.preventDefault();
+            console.log(result);
+            alert('Sua chave secreta foi alterada agora!!');
+
+            $.ajax({
+                method: 'GET',
+                url: urlBase + '/nextChallenge'
+            })
+                .done(function() {
+                    location.reload();
+                });
+
+        });
+    }
 
 
 });
